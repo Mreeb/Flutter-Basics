be5285df12f5647cfe7b4d412c884a5c9d1c4d6e
Muhammad Areeb Khan <103059817+Mreeb@users.noreply.github.com>
2024-09-24 22:40:22 -0700

    Add files via upload

---
+++ Code/main.dart
@@ -0,0 +1,18 @@
+import 'package:coffe_card/Widgets/column_row.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: false),
+      home: const MyColumnRow(),
+    );
+  }
+}
